10c7f56d9d7f23f45e8d4007aec869b51bd010b6
Cindy Grisez <109664955+grisez@users.noreply.github.com>
2024-05-07 14:59:06 +0200

    Update TestService.java

--- src/main/java/com/example/demo/TestService.java
+++ src/main/java/com/example/demo/TestService.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hello {
+public class TestService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
